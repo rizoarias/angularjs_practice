f07dc5fc697d7fc276107e6735a4c18cd765f9f5
Ing. Rizo Arias <83740816+rizoarias@users.noreply.github.com>
2022-03-09 17:06:21 -0600

    data binding y ngmodel

--- Hola_controller/controller.js
+++ Hola_controller/controller.js
@@ -1,4 +1,19 @@
 var app = angular.module('Hola_controller', []);
 app.controller('FirstController', function($scope) {
     $scope.nombre = "Brian";
+    $scope.nuevoComentario = {};
+    $scope.comentarios = [
+        {
+            comentario: "Buen tutorial",
+            username: "brian"
+        },
+        {
+            comentario: "Me gusto mucho",
+            username: "juan"
+        }
+    ];
+    $scope.agregarComentario = function(comentario) {
+        $scope.comentarios.push($scope.nuevoComentario);
+        $scope.nuevoComentario = {};
+    }
 });
